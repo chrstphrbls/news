2226950e93c74a0666effda613f5dbc451c903b2
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-25 13:37:54 +0800

    update-settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig', #initiate users
+    'pages.apps.PagesConfig', #initiate pages app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
