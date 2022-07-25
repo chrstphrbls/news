5858753b5cc617e405d44a06ac33bb144e5e071b
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-25 11:30:07 +0800

    update-setting/redirect url

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser' #initiate custom users
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
