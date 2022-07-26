34dec62408ec95adca6e81fe401e506091099af8
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-26 09:17:25 +0800

    added the model to db

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# articles/models.py
 
-# Create your models here.
+from pyexpat import model
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+
+#'now creating the database for the articles app'
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title 
+
+    def get_absolute_url(self):
+        return reverse("article_detail", args=[str(self.id)])
+    
+
+
